bdec9eb3c702a6ce76b0754404d298b04dd70f56
Samaksh Singh <99samaksh.s@gmail.com>
2024-12-10 11:11:05 +0800

    created Script.py
    Imported libraries and initialized scripts

---
+++ Script/Script.py
@@ -0,0 +1,18 @@
+
+'''
+This tool allows users to open and inspect genome sequence files (FASTA) 
+and genome annotation files (GTF/GFF) through a graphical user interface. 
+It provides a platform for basic genome assembly analysis and file management.
+'''
+
+##########################
+# 1. Importing Libraries #
+##########################
+
+import tkinter as tk 
+from tkinter import filedialog, messagebox, ttk
+from collections import defaultdict
+
+######################################
+# 2. Defining Main Application Class #
+######################################
