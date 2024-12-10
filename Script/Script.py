c06f6e78d90d6b1416d989b3e5b7155c664425e7
Samaksh Singh <99samaksh.s@gmail.com>
2024-12-10 11:21:02 +0800

    Updated Script.py
    Added menu bar function

--- Script/Script.py
+++ Script/Script.py
@@ -5,17 +5,17 @@ and genome annotation files (GTF/GFF) through a graphical user interface.
 It provides a platform for basic genome assembly analysis and file management.
 '''
 
-##########################
-# 1. Importing Libraries #
-##########################
+#######################
+# Importing Libraries #
+#######################
 
 import tkinter as tk 
 from tkinter import filedialog, messagebox, ttk
 from collections import defaultdict
 
-######################################
-# 2. Defining Main Application Class #
-######################################
+###################################
+# Defining Main Application Class #
+###################################
 
 class GenomeAssemblyApp(tk.Tk):
     def __init__(self):
@@ -25,6 +25,19 @@ class GenomeAssemblyApp(tk.Tk):
         self.scaffold_map = {} # Empty dictionary to Stores Scaffold and sequences
         self.create_menu() # Call method for creating menu bar
         self.create_widgets() # call method for creating widgets
+
+    #####################
+    # Creating Menu Bar #
+    #####################
+
+    def create_menu(self):
+
+        menu_bar = tk.Menu(self)
+        file_menu = tk.Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label ="Open FASTA", command =self.open_fasta)
+        file_menu.add_command(label ="Open GTF/GFF", command =self.open_gtf)
+        menu_bar.add_cascade(label = "File", menu = file_menu)
+        self.config(menu = menu_bar)
 
 
 
